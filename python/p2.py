16529e3bd089ab3be5e50242889d528af006f3e5
Shreya Patel <patelshreya.v.1806@gmail.com>
2026-09-11 16:46:27 +0530

    code again changed

--- python/p2.py
+++ python/p2.py
@@ -1,3 +1,6 @@
+
+### pytest example
+
 def add(a,b):
     return a + b
 
@@ -8,3 +11,51 @@ def test_add():
     assert add(-5, -5) == -10
 
 test_add()
+
+
+###email validation
+
+email = input("Enter email: ")
+
+if "@" in email and ".com" in email:
+    print("Valid email")
+else:
+    print("Invalid email")
+
+
+
+
+
+### password validation
+password = input("Enter password: ")
+
+if len(password) < 8:
+    print("Password must be at least 8 characters")
+elif not any(char.isupper() for char in password):
+    print("Password must contain an uppercase letter")
+elif not any(char.islower() for char in password):
+    print("Password must contain a lowercase letter")
+elif not any(char.isdigit() for char in password):
+    print("Password must contain a number")
+else:
+    print("Valid password")
+
+
+
+### validation and exapction handlingtry:
+
+try:
+    age = int(input("Enter age: "))
+    print("Age:", age)
+
+except ValueError:
+    print("Please enter a valid number")
+
+
+    
+if age < 0:
+        print("Age cannot be negative")
+elif age < 120:
+        print("Valid age")
+else:
+        print("Age cannot be greater than 120")
