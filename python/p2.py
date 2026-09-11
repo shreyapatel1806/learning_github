36981cc5d07d85977d01b8374528d1518504645c
Shreya Patel <patelshreya.v.1806@gmail.com>
2026-09-11 16:05:52 +0530

    change

--- python/p2.py
+++ python/p2.py
@@ -1,14 +1,10 @@
-
-def add(a, b):
+def add(a,b):
     return a + b
 
-print(add(10,20))
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+    assert add(-5, -5) == -10
 
-
-
-
-
-def add(a, b):
-    return a + b
-result = add(10, 20)
-print(result)
+test_add()
